81995f8e20517364ab4d698632cf45e59942899d
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2025-10-24 12:57:56 +0300

    generated register component

--- DevTrackerWeb/src/app/features/auth/auth.module.ts
+++ DevTrackerWeb/src/app/features/auth/auth.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
